038e4d6aa9ecac61e500ca3f2042e3cfca1f2462
WDC1931 <674275593@qq.com>
2019-04-17 19:10:59 +0800

    201904171910

--- component-backStage/apis/sport.js
+++ component-backStage/apis/sport.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 var router = express.Router();
 const sportList = require("../models/sport.model");
+const login = require('../controllers/wechat/login');
+
+router.post('/', login);
+router.all('/', function(req, res, next){
+  res.sendStatus(403);
+});
 
 // 保存信息
 router.post("/save", function(req, res, next) {
     var body = req.body;
-  
     sportList
       .create({
         home: body.home,
---
+++ component-backStage/controllers/wechat/login.js
@@ -0,0 +1,6 @@
+module.exports = function(req, res, next) {
+  // 翼企动小程序
+  let appID = `wx1dd3324da7aa888e`;
+  let appSerect = `4a69e014573c4e64f516c464643cde4e`;
+  res.send("重定向成功！");
+};
